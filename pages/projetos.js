1cf956fc44ffefe5f91585ec60074b900a683973
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-10-20 20:41:26 -0300

    JavaScript Clock

--- pages/projetos.js
+++ pages/projetos.js
@@ -1,8 +1,8 @@
 function criar(){
 	projetos = {
-		nomes: ["JavaScript Tests", "Discord Profile", "Em breve"],
-		caminhos: ["https://github.com/victor-4guiar/JavaScript-Tests", "https://victor-4guiar.github.io/Discord-Profile/", "#"],
-		thumbs: ["../images/thumbs/javascript-tests.png", "../images/thumbs/discord-profile.png", "../images/thumbs/beta.png"]
+		nomes: ["JavaScript Tests", "Discord Profile", "JavaScript Clock"],
+		caminhos: ["https://github.com/victor-4guiar/JavaScript-Tests", "https://victor-4guiar.github.io/Discord-Profile/", "https://victor-4guiar.github.io/clock/"],
+		thumbs: ["../images/thumbs/javascript-tests.png", "../images/thumbs/discord-profile.png", "../images/thumbs/clock.png"]
 	}
 	
 	for(let c=0; c<projetos.nomes.length;c++){
