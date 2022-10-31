40694b7f626ec19cd3d822005444dd67710c24d7
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-10-31 17:14:04 -0300

    contato em reforma

---
+++ pages/contato.js
@@ -0,0 +1,9 @@
+window.alert('Aviso! Esta página está em manutenção.')
+
+let ret = document.getElementById('return');
+
+function apresentar(valor){
+	if(valor == 'discord'){
+		ret.innerHTML = '<p class="textli">Rozavick#7866</p>';
+	}
+}
