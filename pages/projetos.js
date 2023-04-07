44742f02ec257a2c09e41beb3b2897053b9bffc0
Victor Aguiar <victoraguiarrosa397@gmail.com>
2023-04-07 14:19:45 -0300

    ajustes

--- pages/projetos.js
+++ pages/projetos.js
@@ -1,46 +1,42 @@
-function criar(){
-	projetos = {
-		nomes: [
-		"Sign Up", 
-		"Discord Profile", 
-		"ClockJS", 
-		"Twitter Profile",
-		"Pokéquiz",
-		"Calculadora"],
-		
-		caminhos: [
-		"https://victor-front.github.io/signup/", 
-		"https://victor-front.github.io/Discord-Profile/", 
-		"https://victor-front.github.io/clock/", 
-		"https://victor-front.github.io/Twitter-Profile/",
-		"https://victor-front.github.io/pokequiz/",
-		"https://victor-front.github.io/calculadora/"],
-		
-		thumbs: [
-		"../images/thumbs/signup.png", 
-		"../images/thumbs/discord-profile.png", 
-		"../images/thumbs/clock.png", 
-		"../images/thumbs/twitter.png",
-		"../images/thumbs/pokequiz.png",
-		"../images/thumbs/calculadora.png"]
-	}
+projetos = {
+	nomes: [
+	"Sign Up", 
+	"Discord Profile", 
+	"ClockJS", 
+	"Twitter Profile",
+	"Pokéquiz",
+	"Calculadora"],
 	
-	for(let c=0; c<projetos.nomes.length;c++){
-		let projectsContainer = document.getElementById('projects-container');
-		
-		let project = document.createElement('div');
-		project.classList.add('project');
-		project.innerHTML = 
-		`<div class="project">
-			<a href="${projetos.caminhos[c]}" rel="extenal" target="_blank">
+	caminhos: [
+	"https://victor-front.github.io/signup/", 
+	"https://victor-front.github.io/Discord-Profile/", 
+	"https://victor-front.github.io/clock/", 
+	"https://victor-front.github.io/Twitter-Profile/",
+	"https://victor-front.github.io/pokequiz/",
+	"https://victor-front.github.io/calculadora/"],
+	
+	thumbs: [
+	"../images/thumbs/signup.png", 
+	"../images/thumbs/discord-profile.png", 
+	"../images/thumbs/clock.png", 
+	"../images/thumbs/twitter.png",
+	"../images/thumbs/pokequiz.png",
+	"../images/thumbs/calculadora.png"]
+}
+
+const project = (nome, caminho, thumb, projeto) => `
+	<div class="project">
+			<a href="${caminho}" rel="extenal" target="_blank">
 				<div class="ph">
-					<h1>${projetos.nomes[c]}</h1>
+					<h1>${nome}</h1>
 				</div>
-				<div class="pb">
-					<img src="${projetos.thumbs[c]}" alt="${projetos.nomes[0]}">
-				</div>
-			</a>
-		</div>`
-		projectsContainer.appendChild(project);
-	}
+			<div class="pb">
+				<img src="${thumb}" alt="${projeto}">
+			</div>
+		</a>
+	</div>
+`
+
+for(let c=0; c<projetos.nomes.length;c++){
+	document.getElementById('projects-container').innerHTML += project(projetos.nomes[c], projetos.caminhos[c], projetos.thumbs[c], projetos.nomes[c]);
 }
